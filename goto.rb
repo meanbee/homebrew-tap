a1646a98228c033fb43c5c0e0b48c6db930dda91
Nick Jones <nick@nicksays.co.uk>
2014-01-13 20:47:17 +0000

    Fix file indentation

--- goto.rb
+++ goto.rb
@@ -1,28 +1,28 @@
 require 'formula'
 
 class Goto < Formula
-	homepage 'https://github.com/shawesome/goto'
-    url 'http://cdn.meanbee.com/brew/dac/goto-3fbc5bfdb03013f535915ca7912db70c4fcc0be4.tar.gz'
-    sha1 'cb261abe84ee8f75344d8a7bc2a35889dbbce72d'
+  homepage 'https://github.com/shawesome/goto'
+  url 'http://cdn.meanbee.com/brew/dac/goto-3fbc5bfdb03013f535915ca7912db70c4fcc0be4.tar.gz'
+  sha1 'cb261abe84ee8f75344d8a7bc2a35889dbbce72d'
 
-    def install
-    	bin.install "goto"
-    	bin.install "goto_aux"
+  def install
+    bin.install "goto"
+    bin.install "goto_aux"
+  end
+
+  def post_install
+    shell = ENV['SHELL']
+
+    ohai "Attempting to automatically complete installation.."
+
+    if shell =~ /\/zsh$/
+      _install_oneliner_to_file "zsh", "~/.zshrc"
+    elsif shell =~ /\/bash$/
+      _install_oneliner_to_file "bash", "~/.bashrc"
+    else
+      opoo "Your shell was not identified, please follow post installation steps."
     end
-
-	def post_install
-		shell = ENV['SHELL']
-
-		ohai "Attempting to automatically complete installation.."
-
-		if shell =~ /\/zsh$/
-			_install_oneliner_to_file "zsh", "~/.zshrc"
-		elsif shell =~ /\/bash$/
-			_install_oneliner_to_file "bash", "~/.bashrc"
-		else
-			opoo "Your shell was not identified, please follow post installation steps."
-		end
-	end
+  end
 
   def caveats; <<-EOS
 Open a new window to use goto!
@@ -39,21 +39,21 @@ EOS
   end
 
   protected
-  	def _shell_func
-  		<<-FUNC
+    def _shell_func
+      <<-FUNC
 function goto {
     eval $(python #{prefix}/bin/goto_aux \$*)
 }
-  		FUNC
-  	end
+      FUNC
+    end
 
-  	def _shell_func_oneliner
-  		_shell_func.gsub(/\s{2,}/, ' ').gsub(/\n/, ' ')
-  	end
+    def _shell_func_oneliner
+      _shell_func.gsub(/\s{2,}/, ' ').gsub(/\n/, ' ')
+    end
 
-  	def _install_oneliner_to_file(shell_name, file)
-		ohai "Detected #{shell_name}! Appending configuration to #{file}"
-		system "echo '" + _shell_func_oneliner() + "' >> #{file}"
-		ohai "Installation complete. Open a new shell session!"
-  	end
+    def _install_oneliner_to_file(shell_name, file)
+      ohai "Detected #{shell_name}! Appending configuration to #{file}"
+      system "echo '" + _shell_func_oneliner() + "' >> #{file}"
+      ohai "Installation complete. Open a new shell session!"
+    end
 end
